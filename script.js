796132e1bfd30fb8ff224d4723ec6def977fb463
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2023-11-15 12:30:10 +0500

    Add js codes and some styles

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  // Set the first accordion as open by default
+  if (i === 1) {
+    acc[i].classList.add("active");
+    var panel = acc[i].nextElementSibling;
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+
   acc[i].addEventListener("click", function() {
+    // Close all other accordions
+    for (var j = 0; j < acc.length; j++) {
+      if (acc[j] !== this) {
+        acc[j].classList.remove("active");
+        var otherPanel = acc[j].nextElementSibling;
+        otherPanel.style.maxHeight = null;
+      }
+    }
+
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -12,3 +28,4 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
